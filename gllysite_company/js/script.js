4361edce97afbfb6767873f61434fb78c04c193c
jiyun0204 <bomchoon0204@gmail.com>
2021-05-28 13:34:34 +0900

    jiyunm
    up

--- gllysite_company/js/script.js
+++ gllysite_company/js/script.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
  $(".slider").bxSlider({
      pager:false,
  });
+
+        $(".modal").colorbox({rel:"modal"});
+        $(".youtube").colorbox({
+              iframe:true,
+            innerWidth:700,
+            innerHeight:393.75
+        })
+
+
+
 });
