e83185dec062ad32817f95a44e4ced2408d7eaee
jiyun0204 <bomchoon0204@gmail.com>
2021-06-08 18:03:30 +0900

    jiyun
    uppae

--- gllysite_company/js/script.js
+++ gllysite_company/js/script.js
@@ -1,15 +1,40 @@
-$(document).ready(function(){
- $(".slider").bxSlider({
-     pager:false,
- });
+$(document).ready(function() {
 
-        $(".modal").colorbox({rel:"modal"});
-        $(".youtube").colorbox({
-              iframe:true,
-            innerWidth:700,
-            innerHeight:393.75
-        })
+  // 반응형
+  var mWidth = $(window).width();
+  if (mWidth <= 768) {
+    $('.main_menu').addClass('mobile');
+    $('.ticon').addClass('mobile');
+  } else {
+    $('.main_menu').removeClass('mobile');
+    $('.ticon').removeClass('mobile');
+  }
 
+
+  // colorbox
+  $(".modal").colorbox({
+    rel: "modal"
+  });
+  $(".youtube").colorbox({
+    iframe: true,
+    innerWidth: 700,
+    innerHeight: 393.75
+  })
+
+});
+
+// 반응형 제어
+$(window).resize(function() {
+
+  // gnb
+  var mWidth = $(window).width();
+  if (mWidth <= 768) {
+    $('.main_menu').addClass('mobile');
+    $('.ticon').addClass('mobile');
+  } else {
+    $('.main_menu').removeClass('mobile');
+    $('.ticon').removeClass('mobile');
+  }
 
 
 });
